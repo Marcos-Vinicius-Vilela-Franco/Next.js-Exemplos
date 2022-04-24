52095d90e46cc1cebc6e065f0d9c1775bf3aec6a
Marcos Vinicius <mvmarcos74@gmail.com>
2022-04-24 15:57:23 -0300

    exemplo next.js

---
+++ src/components/Login.jsx
@@ -0,0 +1,28 @@
+import Style from '../styles/Main.module.css'
+import {useForm} from 'react-hook-form'
+export default function Login() {
+    const {register,handleSubmit} = useForm();
+
+   function handleSignIn(data){
+    console.log(data)
+    }
+    return (
+        <div className={Style.frame}>
+            <form className={Style.home} onSubmit={handleSubmit(handleSignIn)}>
+                <h3 className="mb-3">Sign In</h3>
+                <div className="form-group">
+                    <label>Email </label>
+                    <input {...register('email')} type="email" className="form-control mt-2 mb-2" placeholder="Digite seu email" />
+                </div>
+                <div className="form-group">
+                    <label>Senha</label>
+                    <input {...register('senha')}type="password" className="form-control mt-2 " placeholder="Digite sua senha" />
+                </div>
+                <button type="submit" className="btn btn-primary btn-block mt-3 mb-2">Entrar</button>
+                <p className="forgot-password text-right ">
+                    Esqueceu a <a href="#">senha?</a>
+                </p>
+            </form>
+        </div>
+    )
+}
